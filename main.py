aa5a8c67ff9c7ca83611e2f31d97a48b2d7f4ebd
Nikhil Londhe <57909804+nikhil-co@users.noreply.github.com>
2021-08-21 13:02:12 +0000

    BOT running

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import discord
+import os
 
-client = discord.client()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Yellloo Coldi bot is here',client)
+
+@client.event
+async def on_message(message):
+  command = message.content.split('*cb ')
+
+  if message.author == client.user:
+    return None
+  
+  if command[1] == ('hi'):
+    await message.channel.send("ColdiBot to your service")
+
+# Token to run the bot
+TOKEN = os.environ['TOKEN']
+client.run(TOKEN)
