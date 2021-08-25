1b85661dbbd14de91a5990b4f0566663f3edd0cc
Nikhil Londhe <57909804+nikhil-co@users.noreply.github.com>
2021-08-25 04:39:51 +0000

    online fix

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  command = message.content.split('*cb ')
-
-  if message.author == client.user:
-    return None
   
-  if command[1] == ('hi'):
-    await message.channel.send("ColdiBot to your service")
+  command = message.content
+  if command.startswith('$kb'):
+
+    command = message.content.split('*cb ')
+
+    if message.author == client.user:
+      return None
+    
+    if command[1] == ('hi'):
+      await message.channel.send("ColdiBot to your service")
 
 # Token to run the bot
 TOKEN = os.environ['TOKEN']
