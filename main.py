20457377cceb139a1798e7b1d12e48ebc2419ff0
Nikhil Londhe <57909804+nikhil-co@users.noreply.github.com>
2021-08-25 05:29:01 +0000

    final

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 import discord
 import os
 
+from keep_alive import keep_alive
+
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
-  print('Yellloo Coldi bot is here',client)
+    print('Yellloo Coldi bot is here', client)
+
 
 @client.event
 async def on_message(message):
-  
-  command = message.content
-  if command.startswith('$kb'):
 
-    command = message.content.split('*cb ')
+    pre_commands = {'hi': 'Returns a welcome message'}
+    command = message.content
 
-    if message.author == client.user:
-      return None
-    
-    if command[1] == ('hi'):
-      await message.channel.send("ColdiBot to your service")
+    if command.startswith('*cb'):
+
+        command = message.content.split('*cb ')
+
+        if message.author == client.user:
+          return None
+
+        elif command[1] == ('hi'):
+          await message.channel.send("ColdiBot to your service")
+          return None
+
+        elif command[1] == ('what'):
+
+          await message.channel.send(file=discord.File('gifs/giphy_what.gif'))
+          return None
+
+        else:
+            await message.channel.send("The command " + command[1] +
+                                       " not available")
+            await message.channel.send("Commands that are avalaible: ")
+            ans = str(pre_commands).replace("{", "").replace("}", "").replace(
+                '"', "").replace(",", "").replace("'","")
+
+            await message.channel.send(ans)
+            return None
+
+
+keep_alive() #Fucntion to keep the bot running all the time
 
 # Token to run the bot
 TOKEN = os.environ['TOKEN']
