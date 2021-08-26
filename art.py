027522159c710a21f945e34309852713c228ace0
Nikhil Londhe <57909804+nikhil-co@users.noreply.github.com>
2021-08-26 09:20:54 +0000

    Changed to BOT client and added ability to play audio

--- art.py
+++ art.py
@@ -3,15 +3,13 @@ import os
 
 __name__ = "__main__"
 
-def chat_with_ai(question):
+def chat_with_ai(question,username):
 
   if __name__ == "__main__":
     ai.api_key = os.environ.get('openai_TOKEN') 
     completion = ai.Completion()
 
-    start_chat_log ="""
-    Human: Hello, I am Human.
-    AI: Hello, human I am ColdiBOT.
+    start_chat_log = """Human: Hello, I am"""+str(username)+'.'+"""AI: Hello, human I am ColdiBOT.
     Human: How are you?
     AI: I am fine, thanks for asking. 
     """
@@ -19,7 +17,7 @@ def chat_with_ai(question):
     prompt = f"{start_chat_log}Human: {question}\nAI:"
 
     response = completion.create(prompt = prompt, engine =  "davinci", temperature = 0.85,top_p=1, frequency_penalty=0, 
-    presence_penalty=0.7, best_of=1,max_tokens=7,stop = "\nHuman: ",n=1)
+    presence_penalty=0.7, best_of=1,max_tokens=15,stop = "\nHuman: ",n=1)
 
     return response.choices[0].text
 
--- main.py
+++ main.py
@@ -1,53 +1,94 @@
 import discord
+from discord.ext import commands
+from discord import FFmpegPCMAudio
 
 import os
 
 from keep_alive import keep_alive
 from art import chat_with_ai
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='*cb ',intents=intents)
 
 @client.event
 async def on_ready():
-    print('Yellloo Coldi bot is here', client)
+    print('Yellloo Coldi bot is here', client.user)
+  
+@client.command()
+async def hello(ctx):
+  await ctx.send("Hello")
+
+@client.command(pass_context=True)
+async def join(ctx):
+  if (ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+    f = str(channel)
+    await ctx.send('ColdiBOT has joined the Voice Channel '+f)
+  else:
+    await ctx.send('You have to join a voice channel first to use this command')
+
+@client.command(pass_context=True)
+async def play(ctx):
+  if (ctx.voice_client):
+    voice = ctx.channel.guild.voice_client
+    print(type(ctx.voice_client))
+    source = FFmpegPCMAudio('sounds/lick.mp3')
+    player = voice.play(source)
+    await ctx.send('Now playing the lick')
+  else:
+    await ctx.send('You have to join a voice channel first to use this command')
+
+@client.command(pass_context=True)
+async def pause(ctx):
+  voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+  if voice.is_playing():
+    voice.pause()
+    await ctx.send('Audio Paused')
+  else:
+    await ctx.send('ColdiBOT is not playing anything')
+
+@client.command(pass_context=True)
+async def resume(ctx):
+  voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
+  if voice.is_paused():
+    voice.resume()
+    await ctx.send('Audio Resumed')
+  else:
+    await ctx.send('Audio is already playing')
+
+@client.command(pass_context=True)
+async def leave(ctx):
+  if (ctx.voice_client):
+    await ctx.guild.voice_client.disconnect()
+    await ctx.send("Disconnected")
+  else:
+    await ctx.send('The bot is not in any voice channel')
+
+@client.command()
+async def hi(ctx):
+  await ctx.send("Hello,ColdiBot to your service")
+
+@client.command()
+async def what(ctx):
+  await ctx.channel.send(file=discord.File('gifs/giphy_what.gif'))
 
 @client.event
 async def on_message(message):
-    pre_commands = {'hi': 'Returns a welcome message'}
-    command = message.content
+  command = message.content
+  if command.startswith('*cb'):
+    command = message.content.split('*cb ')
+    if command[1].startswith('ai'):
+      question = command[1].split('ai ')[1]
+      await message.channel.send(chat_with_ai(question,message.author))
+      return
+    await client.process_commands(message)
 
-    if command.startswith('*cb'):
-
-        command = message.content.split('*cb ')
-
-        if message.author == client.user:
-          return None
-
-        elif command[1] == ('hi'):
-          await message.channel.send("ColdiBot to your service")
-          return None
-
-        elif command[1] == ('what'):
-          await message.channel.send(file=discord.File('gifs/giphy_what.gif'))
-          return None
-
-        elif command[1].startswith('ai'):
-          question = command[1].split('ai ')[1]
-          await message.channel.send(chat_with_ai(question))
-
-        else:
-            await message.channel.send("The command " + command[1] +
-                                       " not available")
-            await message.channel.send("Commands that are avalaible: ")
-            ans = str(pre_commands).replace("{", "").replace("}", "").replace(
-                '"', "").replace(",", "").replace("'","")
-
-            await message.channel.send(ans)
-            return None
 
 keep_alive() #Fucntion to keep the bot running all the time
 
 # Token to run the bot
 TOKEN = os.environ['TOKEN']
-client.run(TOKEN) 
-
+client.run(TOKEN)
